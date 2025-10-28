b195295958e09e436e2cbe5b5a773e5fd5804466
Gathungu Githinji <generalgab630@gmail.com>
2025-10-28 11:19:27 +0300

    humanized

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
       <HowItWorks />
       <WhyItMatters />
       <SustainabilitySection />
-      <HeroSection />
+      <CallToAction />
     </div>
   );
 }
